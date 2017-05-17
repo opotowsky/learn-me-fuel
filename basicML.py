e6fdc38c03810c2926bd11e194bd073e7576d4d9
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2017-05-17 17:48:28 -0500

    scripting it up, getting ready for varying ML aglorithm settings to test them

--- basicML.py
+++ basicML.py
@@ -1,23 +1,25 @@
-
-# coding: utf-8
-
-# In[1]:
+#! /usr/bin/env python
 
 from __future__ import print_function
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.linear_model import RidgeClassifier
-from sklearn.neighbors import KNeighborsRegressor
-from sklearn.linear_model import Ridge
-from sklearn import metrics
+from preds import reactor, enrichment, burnup
 import numpy as np
 import pandas as pd
 import glob
 import os
 
+class LearnSet(object):
+    def __init__(self, nuc_concs, reactor, enrichment, burnup):
+        self.nuc_concs = nuc_concs
+        self.reactor = reactor
+        self.enrichment = enrichment
+        self.burnup = burnup
 
-# In[2]:
 
-# Info for labeling data
+###################################################
+# TODO: Leaving the following global for now; fix!#
+###################################################
+
+# Info for labeling the simulation values in the training set
 pwrburn = (600, 1550, 2500, 3450, 4400, 5350, 6300, 7250, 8200, 9150, 10100, 
            11050, 12000, 12950, 13900, 14850, 15800, 16750, 17700
            )
@@ -43,10 +45,7 @@ train_label = {'ReactorType': ['pwr']*11 + ['bwr']*8 + ['phwr']*2,
                'CoolingInts': [(0.000694, 7, 30, 365.25)]*21
                }
 
-
-# In[3]:
-
-# For labeling the actual values in the testing set
+# Info for labeling the simulated/expected values in the testing set
 t_burns = ((1400, 5000, 11000), (5000, 6120), (1700, 8700, 17000),
            (8700, 9150), (8700, 9150), (2000, 7200, 10800),
            (7200, 8800), (7200, 8800)
@@ -65,17 +64,21 @@ test_label = {'ReactorType': ['phwr']*2 + ['pwr']*3 + ['bwr']*3,
               'CoolingInts': [cool1, cool2, cool1, cool2, cool3, cool1, cool2, cool3]
               }
 
-
-# In[4]:
-
 def format_df(filename):
-    # This is an instance of 
+    """
+    This takes a csv filename and reads the data in as a dataframe, which it 
+    returns as data.
+    """
     data = pd.read_csv(filename).T
     data.columns = data.iloc[0]
     data.drop(data.index[0], inplace=True)
     return data
 
 def get_labels(filename, rxtrs):
+    """
+    This takes a filename and a testing or training label, and searches for all 
+    the expected labels for prediction; returns a dict of this information.
+    """
     tail, _ = os.path.splitext(os.path.basename(filename))
     i = rxtrs['OrigenReactor'].index(tail)
     rxtr_info = {'ReactorType': rxtrs['ReactorType'][i], 
@@ -86,14 +89,25 @@ def get_labels(filename, rxtrs):
     return rxtr_info
 
 def label_data(label, data):
+    """
+    Takes the label for a given simulation in the form of a dict and a dataframe
+    of the simulation results; adds these labels as additional columns to the 
+    dataframe and returns another dataframe.
+    """
     col = len(data.columns)
     data.insert(loc = col, column = 'ReactorType', value = label['ReactorType'])
     data.insert(loc = col+1, column = 'Enrichment', value = label['Enrichment'])
-    burnup = burnup_label(data, label['Burnup'], label['CoolingInts'])
+    burnup = burnup_label(label['Burnup'], label['CoolingInts'])
     data.insert(loc = col+2, column = 'Burnup', value = burnup)
     return data
 
-def burnup_label(data, burn_steps, cooling_ints):
+def burnup_label(burn_steps, cooling_ints):
+    """
+    Takes the number of burnup steps and cooling steps for each case within the 
+    simulation and creates a list of the burnup of the irradiated and cooled/ 
+    decayed fuels; returns a list to be added as the burnup label to the main 
+    dataframe.
+    """
     num_cases = len(burn_steps)
     steps_per_case = len(cooling_ints) + 2
     burnup_list = [0, ]
@@ -110,6 +124,12 @@ def burnup_label(data, burn_steps, cooling_ints):
     return burnup_list
 
 def dataframeXY(all_files, rxtr_label):
+    """"
+    Takes the glob of files in a directory as well as the reactor label (dict)
+    that represents the relevant inputs to the simulations and produces a 
+    dataframe that has both the data features (X) and labeled data (Y) for each
+    instance.
+    """
     all_data = []
     for f in all_files:
         data = format_df(f)
@@ -119,132 +139,57 @@ def dataframeXY(all_files, rxtr_label):
     dfXY = pd.concat(all_data)
     return dfXY
 
-def top_nucs(df):
-    # for each instance (row), keep only top 200 values, replace rest with 0 (scikit-learn won't accept NaN)
-    top_n = 200
-    ##################################################################
-    # This gave seemingly uncorrelated indices -- needs exploration ##
-    #top_cols = {}
-    #for instance in df.itertuples():
-    #    top_nucs = np.argpartition(instance[1:], -top_n)[-top_n:]
-    #    top_cols[instance[0]] = top_nucs
-    #print(top_cols[2000])
-    #?????cond = [df.iloc[:, df.columns != col] for col in top_cols]
-    #?????df.where(cond, other = np.nan, inplace=True)
-    ##################################################################
-    
-    ########################################################################
-    # This worked but made column lengths different between training sets ##
-    #func = lambda x: x.sort_values(ascending=False)[:top_n]
-    #top_cols = df.apply(func, axis=1)
-    ########################################################################
-    
-    #######################################
-    # This gave KeyError '      10001001'##
-    #cols = list(df.T.columns.values)
-    #top_cols = np.array([df.T[nuc].nlargest(top_n, cols).index.values for nuc, nums in df.T.iterrows()])
-    #######################################
-    
-    #top_cols.fillna(value=0, inplace=True)
-    #print(top_cols)
-    return df
-
 def splitXY(dfXY):
+    """
+    Takes a dataframe with a few input-related columns, nuclide concentrations, 
+    and 3 labels and produces four different dataframes:
+    nuclide concentrations only (input-related columns deleted) + 1 dataframe 
+    for each label column.
+    """
     x = len(dfXY.columns)-3
     y = x
     # Need better way to know when the nuclide columns start (6 for now)
     # Prob will just search for column idx that starts with str(1)?
     dfX = dfXY.iloc[:, 6:x]
-    # Best place to filter for top 200 nuclides: 
+    # Best place to filter for top 200 nuclides is here 
     # (but spent 6 hours trying to figure out and failed)
-    #dfX = top_nucs(dfX)
     r_dfY = dfXY.iloc[:, y]
     e_dfY = dfXY.iloc[:, y+1]
     b_dfY = dfXY.iloc[:, y+2]
     return dfX, r_dfY, e_dfY, b_dfY
 
+def main():
+    """
+    Takes all origen files and compiles them into the appropriate dataframes for 
+    training and testing sets. Then splits those dataframes into the appropriate 
+    X and Ys for prediction of reactor type, fuel enrichment, and burnup.
+    """
+    print("Did you check your training and testing data paths?\n")
+    ### Right now this workflow is copied for the testing set but in the future
+    ### this will be a loop because I will be doing cross-validation.
+    
+    # Training Dataset
+    trainpath = "../origen/origen-data/training/9may2017/csv/"
+    train_files = glob.glob(os.path.join(trainpath, "*.csv"))
+    trainXY = dataframeXY(train_files, train_label)
+    trainXY.reset_index(inplace=True)
+    trainX, r, e, b = splitXY(trainXY)
+    train_set = LearnSet(nuc_concs = trainX, reactor = r, enrichment = e, burnup = b)
+    
+    # Testing Dataset (for now)
+    testpath = "../origen/origen-data/testing/10may2017_2/csv/"
+    test_files = glob.glob(os.path.join(testpath, "*.csv"))
+    testXY = dataframeXY(test_files, test_label)
+    testXY.reset_index(inplace=True)
+    testX, r, e, b = splitXY(testXY)
+    test_set = LearnSet(nuc_concs = testX, reactor = r, enrichment = e, burnup = b)
+    
+    # Predict!
+    reactor(train_set, test_set)
+    enrichment(train_set, test_set)
+    burnup(train_set, test_set)
+    
+    return
 
-# In[5]:
-
-# Training Dataset
-###############################################
-## Still need to filter for top 200 nuclides ##
-###############################################
-trainpath = "../origen/origen-data/training/9may2017/csv/"
-train_files = glob.glob(os.path.join(trainpath, "*.csv"))
-trainXY = dataframeXY(train_files, train_label)
-trainXY.reset_index(inplace=True)
-trainX, r_trainY, e_trainY, b_trainY = splitXY(trainXY)
-# Testing Dataset (for now)
-testpath = "../origen/origen-data/testing/10may2017_2/csv/"
-test_files = glob.glob(os.path.join(testpath, "*.csv"))
-testXY = dataframeXY(test_files, test_label)
-testXY.reset_index(inplace=True)
-testX, r_testY, e_testY, b_testY = splitXY(testXY)
-
-
-# In[6]:
-
-# Reactor Type
-# L1 norm is Manhattan Distance
-# L2 norm is Euclidian Distance 
-# Ridge Regression is Linear + L2 regularization
-l1knc = KNeighborsClassifier(metric='l1', p=1)
-l2knc = KNeighborsClassifier(metric='l2', p=2)
-rc = RidgeClassifier()
-l1knc.fit(trainX, r_trainY)
-l2knc.fit(trainX, r_trainY)
-rc.fit(trainX, r_trainY)
-# Predictions
-predict1 = l1knc.predict(testX)
-predict2 = l2knc.predict(testX)
-predict3 = rc.predict(testX)
-expected = r_testY
-print(metrics.classification_report(expected, predict1))
-print(metrics.classification_report(expected, predict2))
-print(metrics.classification_report(expected, predict3))
-
-
-# In[7]:
-
-# Enrichment
-l1knr = KNeighborsRegressor(metric='l1', p=1)
-l2knr = KNeighborsRegressor(metric='l2', p=2)
-rr = Ridge()
-l1knr.fit(trainX, e_trainY)
-l2knr.fit(trainX, e_trainY)
-rr.fit(trainX, e_trainY)
-predict1 = l1knr.predict(testX)
-predict2 = l2knr.predict(testX)
-predict3 = rr.predict(testX)
-expected = e_testY
-print(metrics.mean_absolute_error(expected, predict1))
-print(metrics.mean_absolute_error(expected, predict2))
-print(metrics.mean_absolute_error(expected, predict3))
-
-
-# In[8]:
-
-# Burnup
-bl1knr = KNeighborsRegressor(metric='l1', p=1)
-bl2knr = KNeighborsRegressor(metric='l2', p=2)
-brr = Ridge()
-bl1knr.fit(trainX, b_trainY)
-bl2knr.fit(trainX, b_trainY)
-brr.fit(trainX, b_trainY)
-predict1 = bl1knr.predict(testX)
-predict2 = bl2knr.predict(testX)
-predict3 = brr.predict(testX)
-expected = b_testY
-print(metrics.mean_absolute_error(expected, predict1))
-print(metrics.mean_absolute_error(expected, predict2))
-print(metrics.mean_absolute_error(expected, predict3))
-print(metrics.r2_score(expected, predict1))
-print(metrics.r2_score(expected, predict2))
-print(metrics.r2_score(expected, predict3))
-
-
-# In[ ]:
-
-
-
+if __name__ == "__main__":
+    main()
---
+++ preds.py
@@ -0,0 +1,85 @@
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import RidgeClassifier
+from sklearn.neighbors import KNeighborsRegressor
+from sklearn.linear_model import Ridge
+from sklearn import metrics
+
+
+def reactor(train, test):
+    """
+    """
+    # Training for Reactor Type
+    # L1 norm is Manhattan Distance
+    # L2 norm is Euclidian Distance 
+    # Ridge Regression is Linear + L2 regularization
+    l1 = KNeighborsClassifier(metric='l1', p=1)
+    l2 = KNeighborsClassifier(metric='l2', p=2)
+    rc = RidgeClassifier()
+    l1.fit(train.nuc_concs, train.reactor)
+    l2.fit(train.nuc_concs, train.reactor)
+    rc.fit(train.nuc_concs, train.reactor)
+    
+    # Predictions
+    predict1 = l1.predict(test.nuc_concs)
+    predict2 = l2.predict(test.nuc_concs)
+    predict3 = rc.predict(test.nuc_concs)
+    expected = test.reactor
+    print(metrics.classification_report(expected, predict1))
+    print(metrics.classification_report(expected, predict2))
+    print(metrics.classification_report(expected, predict3))
+    
+    return
+
+def enrichment(train, test):
+    """
+    """
+    # Training for Enrichment
+    # L1 norm is Manhattan Distance
+    # L2 norm is Euclidian Distance 
+    # Ridge Regression is Linear + L2 regularization
+    l1 = KNeighborsRegressor(metric='l1', p=1)
+    l2 = KNeighborsRegressor(metric='l2', p=2)
+    rr = Ridge()
+    l1.fit(train.nuc_concs, train.enrichment)
+    l2.fit(train.nuc_concs, train.enrichment)
+    rr.fit(train.nuc_concs, train.enrichment)
+    
+    # Predictions
+    predict1 = l1.predict(test.nuc_concs)
+    predict2 = l2.predict(test.nuc_concs)
+    predict3 = rr.predict(test.nuc_concs)
+    expected = test.enrichment
+    print(metrics.mean_absolute_error(expected, predict1))
+    print(metrics.mean_absolute_error(expected, predict2))
+    print(metrics.mean_absolute_error(expected, predict3))
+
+    return
+
+def burnup(train, test):
+    """
+    """
+    # Training for Burnup
+    # L1 norm is Manhattan Distance
+    # L2 norm is Euclidian Distance 
+    # Ridge Regression is Linear + L2 regularization
+    l1 = KNeighborsRegressor(metric='l1', p=1)
+    l2 = KNeighborsRegressor(metric='l2', p=2)
+    rr = Ridge()
+    l1.fit(train.nuc_concs, train.burnup)
+    l2.fit(train.nuc_concs, train.burnup)
+    rr.fit(train.nuc_concs, train.burnup)
+
+    # Predictions
+    predict1 = l1.predict(test.nuc_concs)
+    predict2 = l2.predict(test.nuc_concs)
+    predict3 = rr.predict(test.nuc_concs)
+    expected = test.burnup
+    print(metrics.mean_absolute_error(expected, predict1))
+    print(metrics.mean_absolute_error(expected, predict2))
+    print(metrics.mean_absolute_error(expected, predict3))
+    print(metrics.r2_score(expected, predict1))
+    print(metrics.r2_score(expected, predict2))
+    print(metrics.r2_score(expected, predict3))
+    
+    return
+
