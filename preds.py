69e7868d0bfa8b0d8a3a629a1009a21cff07e6ae
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2017-12-06 18:02:02 -0500

    didn't want to change preds

--- preds.py
+++ preds.py
@@ -1,58 +1,62 @@
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.neighbors import KNeighborsRegressor
 from sklearn.model_selection import cross_val_predict
-from sklearn.model_selection import learning_curve
 from sklearn import metrics
 import numpy as np
 import pandas as pd
 
-TRAINSET_SIZES = np.linspace(0.1, 1.0, 19)
-
-def format(m, cv1, tr1, cv2, tr2):
-    # average cv folds + format
-    m = m.tolist()
-    cv1 = np.array([np.mean(row) for row in cv1]).tolist()
-    tr1 = np.array([np.mean(row) for row in tr1]).tolist()
-    cv2 = np.array([np.mean(row) for row in cv2]).tolist()
-    tr2 = np.array([np.mean(row) for row in tr2]).tolist()
-    score = []
-    for i in m:
-        idx = m.index(i)
-        scr = (cv1[idx], tr1[idx], cv2[idx], tr2[idx])
-        score.append(scr)
-    return score
-
-def classification(X, y, k, cv_fold):
+def classification(X, Y, k, cv_fold):
     """
     Training for Classification: predicts a model using two knn algorithms, 
     returning the training and cross validation accuracies for each. 
     
     """
     
-    # m is training set size
     # L1 norm is Manhattan Distance
     # L2 norm is Euclidian Distance 
     l1 = KNeighborsClassifier(n_neighbors = k, metric='l1', p=1)
     l2 = KNeighborsClassifier(n_neighbors = k, metric='l2', p=2)
-    _, trl1, cvl1 = learning_curve(l1, X, y, train_sizes=TRAINSET_SIZES, cv=cv_fold)
-    m, trl2, cvl2 = learning_curve(l2, X, y, train_sizes=TRAINSET_SIZES, cv=cv_fold)
-    accuracy = format(m, cvl1, trl1, cvl2, trl2)
+    l1_pred = cross_val_predict(l1, X, Y, cv=cv_fold)
+    l2_pred = cross_val_predict(l2, X, Y, cv=cv_fold)
+    # for diagnostic curves 
+    l1.fit(X, Y)
+    l2.fit(X, Y)
+    tr1_pred = l1.predict(X)
+    tr2_pred = l2.predict(X)
+    # Accuracies
+    cvl1 = metrics.accuracy_score(Y, l1_pred)
+    cvl2 = metrics.accuracy_score(Y, l2_pred)
+    trl1 = metrics.accuracy_score(Y, tr1_pred)
+    trl2 = metrics.accuracy_score(Y, tr2_pred)
+    # accuracies for csv
+    accuracy = (cvl1, trl1, cvl2, trl2)
     return accuracy
 
-def regression(X, y, k, cv_fold):
+def regression(X, Y, k, cv_fold):
     """ 
     Training for Regression: predicts a model using two knn algorithms, 
     returning the training and cross validation errors for each. 
 
     """
-    # m is training set size
+    
     # L1 norm is Manhattan Distance
     # L2 norm is Euclidian Distance 
     l1 = KNeighborsRegressor(n_neighbors = k, metric='l1', p=1)
     l2 = KNeighborsRegressor(n_neighbors = k, metric='l2', p=2)
-    _, trl1, cvl1 = learning_curve(l1, X, y, train_sizes=TRAINSET_SIZES, cv=cv_fold, scoring='neg_mean_squared_error')
-    m, trl2, cvl2 = learning_curve(l2, X, y, train_sizes=TRAINSET_SIZES, cv=cv_fold, scoring='neg_mean_squared_error')
-    mse = format(m, cvl1, trl1, cvl2, trl2)
+    l1_pred = cross_val_predict(l1, X, Y, cv=cv_fold)
+    l2_pred = cross_val_predict(l2, X, Y, cv=cv_fold)
+    # for diagnostic curves
+    l1.fit(X, Y)
+    l2.fit(X, Y)
+    tr1_pred = l1.predict(X)
+    tr2_pred = l2.predict(X)
+    # Errors
+    cvl1 = metrics.mean_squared_error(Y, l1_pred)
+    cvl2 = metrics.mean_squared_error(Y, l2_pred)
+    trl1 = metrics.mean_squared_error(Y, tr1_pred)
+    trl2 = metrics.mean_squared_error(Y, tr2_pred)
+    # mean squared error for csv
+    mse = (cvl1, trl1, cvl2, trl2)
     return mse
 
 def train_and_predict(X, rY, cY, eY, bY):
@@ -77,18 +81,48 @@ def train_and_predict(X, rY, cY, eY, bY):
 
     """
 
+    # regularization parameters (k's differ for each type of prediction)
+    #reg = {'r_l1' : 15, 'r_l2' : 20, 
+    #       'c_l1' : 7, 'c_l2' : 10, 
+    #       'e_l1' : 7, 'e_l2' : 10, 
+    #       'b_l1' : 30, 'b_l2' : 35,
+    #       }
     cv_folds = 10
-    k = 2
-    trainsize = TRAINSET_SIZES
-    reactor_acc  = classification(X, rY, k, cv_folds)
-    cooling_err= regression(X, cY, k, cv_folds)
-    enrichment_err = regression(X, eY, k, cv_folds)
-    burnup_err = regression(X, bY, k, cv_folds)
+    klist = list(range(1, 50))
+    reactor_acc = []
+    cooling_err = []
+    enrichment_err = []
+    burnup_err = []
+    for k in klist:
+        r = classification(X, rY, k, cv_folds)
+        c = regression(X, cY, k, cv_folds)
+        e = regression(X, eY, k, cv_folds)
+        b = regression(X, bY, k, cv_folds)
+        reactor_acc.append(r)
+        cooling_err.append(c)
+        enrichment_err.append(e)
+        burnup_err.append(b)
+
+
+#            r_sum = [sum(x) for x in zip(r, r_sum)]
+#            c_sum = [sum(x) for x in zip(c, c_sum)]
+#            e_sum = [sum(x) for x in zip(e, e_sum)]
+#            b_sum = [sum(x) for x in zip(b, b_sum)]
+#        # Take Average
+#        reactor = [x / n_trials for x in r_sum]
+#        cooling = [x / n_trials for x in c_sum]
+#        enrichment = [x / n_trials for x in e_sum]
+#        burnup = [x / n_trials for x in b_sum]
+#        reactor_acc.append(reactor)
+#        cooling_err.append(cooling)
+#        enrichment_err.append(enrichment)
+#        burnup_err.append(burnup)
+    
     # Save results
     cols = ['CVL1', 'TrainL1', 'CVL2', 'TrainL2']
-    pd.DataFrame(reactor_acc, columns=cols, index=trainsize).to_csv('reactor.csv')
-    pd.DataFrame(cooling_err, columns=cols, index=trainsize).to_csv('cooling.csv')
-    pd.DataFrame(enrichment_err, columns=cols, index=trainsize).to_csv('enrichment.csv')
-    pd.DataFrame(burnup_err, columns=cols, index=trainsize).to_csv('burnup.csv')
+    pd.DataFrame(reactor_acc, columns=cols, index=klist).to_csv('reactor.csv')
+    pd.DataFrame(cooling_err, columns=cols, index=klist).to_csv('cooling.csv')
+    pd.DataFrame(enrichment_err, columns=cols, index=klist).to_csv('enrichment.csv')
+    pd.DataFrame(burnup_err, columns=cols, index=klist).to_csv('burnup.csv')
 
     return 
