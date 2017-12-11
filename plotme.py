f480c36a8db89fea07f74432ccb6d89c962663a4
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2017-12-11 16:59:07 -0500

    just need some tweaks but have to retrain everything first

--- plotme.py
+++ plotme.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python3
+#! /usr/bin/env python
 
 import matplotlib.pyplot as plt
 import pandas as pd
@@ -11,17 +11,18 @@ plt.rcParams['font.family'] = 'serif'
 #plt.rcParams['font.serif'] = 'Ubuntu'
 #plt.rcParams['font.monospace'] = 'Ubuntu Mono'
 plt.rcParams['font.size'] = 14
-plt.rcParams[''nes.linewidth] = 2.5
-plt.rcParams['axes.labelsize'] = 14
+plt.rcParams['lines.linewidth'] = 2.5
+plt.rcParams['axes.labelsize'] = 12
 plt.rcParams['axes.labelweight'] = 'bold'
-plt.rcParams['axes.titlesize'] = 14
-plt.rcParams['xtick.labelsize'] = 14
-plt.rcParams['ytick.labelsize'] = 14
-plt.rcParams['legend.fontsize'] = 14
+plt.rcParams['axes.titlesize'] = 12
+plt.rcParams['xtick.labelsize'] = 12
+plt.rcParams['ytick.labelsize'] = 12
+plt.rcParams['legend.fontsize'] = 12
+plt.rcParams['figure.titlesize'] = 14
 #plt.rcParams['figure.figsize'] = ()
 
 # Custom Color Blind Palette
-BROWN = '#8c510a'
+DBROWN = '#8c510a'
 MBROWN = '#d8b365'
 LBROWN = '#f6e8c3'
 DTEAL = '#01665e'
@@ -32,57 +33,63 @@ MPURPLE = '#af8dc3'
 LPURPLE = '#e7d4e8'
 
 # Where the data are stored:
-preds = ['reactor', 'enrichment', 'cooling', 'burnup']
-src = ['_nucs', '_gammas']
+rpath = './results/8dec_knn/fissionprods/'
+preds = 'reactor', 'cooling', 'enrichment', 'burnup')
+src = ('_nucs', '_gammas')
 ###########################################
 ######### These two are hardcoded #########
-rpath = './results/8dec_knn/fissionprods/'#
-subset = '_fiss'                          #
-tsubset = 'Fission Products'              #
+subset = '_fissact'
+tsubset = 'Fission Products and Actinides'
 ###########################################
 print('You are plotting the {} subset'.format(subset), flush=True)
 # Other useful lists for plotting
-colors = [DBROWN, MBROWN, DTEAL, MTEAL]
-labels = ['CVL1', 'TrainL1', 'CVL2', 'TrainL2']     
-quality = ['Nuclide Concentrations', 'Gamma spectra']
-titles = ['Reactor Type', 'Enrichment [%U235]', 
-          'Burnup [MWd/MTU]', 'Cooling Time [min?]'
-          ]
+colors = (DBROWN, MBROWN, DTEAL, MTEAL)
+labels = ('CVL1', 'TrainL1', 'CVL2', 'TrainL2')
+quality = ('Nuclide Concentrations', 'Gamma spectra')
+titles = ('Reactor Type', 'Cooling Time [days?]', 
+          'Enrichment [%U235]', 'Burnup [?Wd/MTU]'
+          )
 
 # Learning Curve Data
-for i, s in enumerate(src):
-    for j, p in enumerate(preds):
-        axl = plt.subplot()  
+for s in src:
+    i = src.index(s)
+    for p in preds:
+        j = preds.index(p)
+        fig = plt.figure()  
         lcsv = 'lc_' + p + subset + s + '.csv'
         ldatapath = os.path.join(rpath, lcsv)
         ldata = pd.read_csv(ldatapath)
+        X = ldata.iloc[:, 0]
         plt.xlabel('Training set size (m)')
         # y labels
         if j == 0:
             plt.ylabel('Accuracy Score')
         else:
             plt.ylabel('Negative Mean-squared Error')
-        for lrank, column in enumerate(ldata.columns):  
-            ldata[column].plot(ldata.X, ldata.Y, ax=axl, color=colors[lrank])
+        for column in labels:
+            Y = ldata.loc[:, column]
+            plt.plot(X, Y, label=column, color=colors.index(column))
+        leg=plt.legend(loc='best', fancybox=True)
         # Customize title location
-        plt_title = 'Learning Curve: ' + titles[j] + ' Predictions from ' + 
-                    tsubset + ' of ' + quality[i]
-        plt.text(0.5*(left+right), top, plt_title, fontsize=16, ha='center', va='bottom')
+        plt_title = 'Learning Curve: ' + titles[j] + ' Predictions from ' + \
+                     tsubset + ' of ' + quality[i]
+        plt.title(plt_title, fontstyle='italic')
         # Save figure as PNG
         filename = 'lc_' + p + subset + s + '.png'
         plt.savefig(filename, bbox_inches="tight")
+        plt.close(fig)
 
         
-# Validation Curve Data
-for i, s in enumerate(src):
-    for j, p in enumerate(preds):
-        fig = plt.figure()  
-        vcsv = 'vc_' + p + subset + s + '.csv'
-        vdatapath = os.path.join(rpath, vcsv)
-        vdata = pd.read_csv(vdatapath)
-        plt.xlabel('Neighborhood size (k)')
-        # y labels
-        if j == 0:
-            plt.ylabel('Accuracy Score')
-        else:
-            ply.ylabel('Negative Mean-squared Error')
+## Validation Curve Data
+#for i, s in enumerate(src):
+#    for j, p in enumerate(preds):
+#        fig = plt.figure()  
+#        vcsv = 'vc_' + p + subset + s + '.csv'
+#        vdatapath = os.path.join(rpath, vcsv)
+#        vdata = pd.read_csv(vdatapth)
+#        plt.xlabel('Neighborhood size (k)')
+#        # y labels
+#        if j == 0:
+#            plt.ylabel('Accuracy Score')
+#        else:
+#            ply.ylabel('Negative Mean-squared Error')
