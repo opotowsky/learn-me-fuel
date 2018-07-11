eec32701c41283ddf29da0a7c04c58e7de268ed5
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2018-07-11 15:08:52 -0500

    tools.py file since more than one script uses the same functions

---
+++ tools.py
@@ -0,0 +1,33 @@
+#! /usr/bin/env python3
+
+def splitXY(dfXY):
+    """
+    Takes a dataframe with all X (features) and Y (labels) information and 
+    produces five different pandas datatypes: a dataframe with nuclide info 
+    only + a series for each label column.
+
+    Parameters
+    ----------
+    dfXY : dataframe with nuclide concentraations and 4 labels: reactor type, 
+           cooling time, enrichment, and burnup
+
+    Returns
+    -------
+    dfX : dataframe with only nuclide concentrations for each instance
+    rY : dataframe with reactor type for each instance
+    cY : dataframe with cooling time for each instance
+    eY : dataframe with fuel enrichment for each instance
+    bY : dataframe with fuel burnup for each instance
+
+    """
+
+    lbls = ['ReactorType', 'CoolingTime', 'Enrichment', 'Burnup', 'OrigenReactor']
+    dfX = dfXY.drop(lbls, axis=1)
+    if 'total' in dfX.columns:
+        data.drop('total', axis=1, inplace=True)
+    r_dfY = dfXY.loc[:, lbls[0]]
+    c_dfY = dfXY.loc[:, lbls[1]]
+    e_dfY = dfXY.loc[:, lbls[2]]
+    b_dfY = dfXY.loc[:, lbls[3]]
+    return dfX, r_dfY, c_dfY, e_dfY, b_dfY
+
