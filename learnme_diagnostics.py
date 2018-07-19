e0bcefa1ddd9df502103c2df49db2bc300c207f4
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2018-07-18 19:29:38 -0500

    added learning curve function to script

--- learnme_diagnostics.py
+++ learnme_diagnostics.py
@@ -17,48 +17,66 @@ import os
 
 
 
-#def learning_curves(X, rY, cY, eY, bY, nuc_subset):
-#    """
-#    
-#    Given training data, iteratively runs some ML algorithms (currently, this
-#    is nearest neighbor, linear, and support vector methods), varying the
-#    training set size for each prediction category: reactor type, cooling
-#    time, enrichment, and burnup
-#
-#    Parameters 
-#    ---------- 
-#    
-#    X : dataframe that includes all training data
-#    *Y : series with labels for training data
-#    nuc_subset : one of all, fission products, actinides, 
-#                 or fission products with actinides
-#
-#    Returns
-#    -------
-#    learning_reactor*.csv : 
-#    learning_cooling*.csv : 
-#    learning_enrichment*.csv : 
-#    learning_burnup*.csv : 
-#
-#    """    
-#    TRAINSET_SIZES = np.linspace(0.1, 1.0, 19)
-#    m_percent = np.linspace(0.15, 1.0, 20)
-#    alpha = 0.1
-#    rxtr_lc  = lc_classification(X, rY, alpha, cv_folds)
-#    cool_lc= lc_regression(X, cY, alpha, cv_folds)
-#    enr_lc = lc_regression(X, eY, alpha, cv_folds)
-#    burn_lc = lc_regression(X, bY, alpha, cv_folds)
-#    r_lc = 'lc_' + 'reactor' + nucs_tracked + train_src + '.csv'
-#    c_lc = 'lc_' + 'cooling' + nucs_tracked + train_src + '.csv'
-#    e_lc = 'lc_' + 'enrichment' + nucs_tracked + train_src + '.csv'
-#    b_lc = 'lc_' + 'burnup' + nucs_tracked + train_src + '.csv'
-#    idx_lc = TRAINSET_SIZES
-#    pd.DataFrame(rxtr_lc, columns=cols, index=idx_lc).to_csv(r_lc)
-#    pd.DataFrame(cool_lc, columns=cols, index=idx_lc).to_csv(c_lc)
-#    pd.DataFrame(enr_lc, columns=cols, index=idx_lc).to_csv(e_lc)
-#    pd.DataFrame(burn_lc, columns=cols, index=idx_lc).to_csv(b_lc)
-#    
-#    return 
+def learning_curves(X, Y, knn, rr, svr, scores, kfold, csv_name):
+    """
+    
+    Given training data, iteratively runs some ML algorithms (currently, this
+    is nearest neighbor, linear, and support vector methods), varying the
+    training set size for each prediction category: reactor type, cooling
+    time, enrichment, and burnup
+
+    Parameters 
+    ---------- 
+    
+    X : dataframe that includes all training data
+    Y : series with labels for training data
+    knn : initialized kNN learner
+    rr : initialized RR learner
+    svr : initialized SVR learner
+    scores : list of scoring types (from sckikit-learn)
+    kfold : cross-validation generator
+    csv_name : string containing the train set, nuc subset, and parameter being 
+               predicted for naming purposes
+
+    Returns
+    -------
+    *learning_curve.csv : csv file with learning curve results for each 
+                          prediction category
+
+    """    
+    m = np.array( [0.1, 0.2, 0.3, 0.4, 0.5, 0.55, 0.6, 0.65, 
+                   0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1.0]
+                 )
+    fname = csv_name + 'learning_curve.csv'
+    lc_data = pd.DataFrame()
+    col_names = ['FracTrainSize', 'AbsTrainSize', 'TrainScore', 'CV-Score',
+                 'Algorithm', 'ScoringMetric']
+    
+    for alg_type in ('knn', 'rr', 'svr'):
+        if alg_type == 'knn':
+            alg = knn
+        elif alg_type == 'rr':
+            alg = rr
+        else:
+            alg = svr
+
+        for score in scores:
+            tsize, train, cv = learning_curve(alg, X, Y, train_sizes=m, cv=kfold, 
+                                              scoring=score, shuffle=True)
+            train_mean = np.mean(train, axis=1)
+            cv_mean = np.mean(cv, axis=1)
+            lc_data['FracTrainSize'] = m
+            lc_data['AbsTrainSize'] = tsize
+            lc_data['TrainScore'] = train_mean
+            lc_data['CV-Score'] = cv_mean
+            lc_data['Algorithm'] = alg_type
+            lc_data['ScoringMetric'] = score
+            if not os.path.isfile(fname):
+                lc_data.to_csv(fname, mode='w', header=col_names)
+            else:
+                lc_data.to_csv(fname, mode='a', header=False)
+    
+    return 
 
 def validation_curves(X, Y, knn, rr, svr_g, svr_c, scores, kfold, csv_name):
     """
@@ -72,17 +90,20 @@ def validation_curves(X, Y, knn, rr, svr_g, svr_c, scores, kfold, csv_name):
     ---------- 
     
     X : dataframe that includes all training data
-    *Y : series with labels for training data
+    Y : series with labels for training data
+    knn : initialized kNN learner
+    rr : initialized RR learner
+    svr_g : initialized SVR learner with constant C
+    svr_c : initialized SVR learner with constant gamma
     scores : list of scoring types (from sckikit-learn)
-    kfold : 
-    nuc_subset : one of all, fission products, actinides, 
-                 or fission products with actinides
-    trainset : integer corresponding to training set
+    kfold : cross-validation generator
+    csv_name : string containing the train set, nuc subset, and parameter being 
+               predicted for naming purposes
 
     Returns
     -------
-    *_validation.csv : csv file with validation curve results for each 
-                       prediction category
+    *validation_curve.csv : csv file with validation curve results for each 
+                            prediction category
 
     """    
     # Varied alg params for validation curves
@@ -153,14 +174,14 @@ def main():
 
 
     pkl_base = './pkl_trainsets/2jul2018/2jul2018_trainset'
-    for trainset in ('1', '2'):
-        for subset in ('fiss', 'act', 'fissact', 'all'):
+    for trainset in ('1',):# '2'):
+        for subset in ('fissact',):# 'act', 'fiss', 'all'):
             pkl = pkl_base + trainset + '_nucs_' + subset + '_not-scaled.pkl'
             trainXY = pd.read_pickle(pkl, compression=None)
             
             trainX, rY, cY, eY, bY = splitXY(trainXY)
             if subset == 'all':
-                top_n = 100
+                top_n = 50
                 nuc_set = top_nucs(trainX, top_n)
                 trainX = filter_nucs(trainX, nuc_set, top_n)
 
@@ -169,7 +190,7 @@ def main():
             trainX = scale(trainX) 
             
             # loops through each reactor parameter to do separate predictions
-            for Y in ('c', 'e', 'b', 'r'):
+            for Y in ('r', 'b', 'c', 'e'):
                 trainY = pd.Series()
                 # get param names and set ground truth
                 if Y == 'c':
@@ -186,35 +207,36 @@ def main():
                     parameter = 'reactor'
                 
                 if Y is not 'r':
-                    scores = ['r2', 'explained_variance', 'neg_mean_absolute_error', 'neg_mean_squared_error']
+                    #scores = ['r2', 'explained_variance', 'neg_mean_absolute_error', 'neg_mean_squared_error']
+                    scores = ['explained_variance', 'neg_mean_absolute_error']
                     kfold = KFold(n_splits=CV, shuffle=True)
-                    knn_init_vc = KNeighborsRegressor(weights='distance')
-                    rr_init_vc = Ridge()
-                    svr_g_init_vc = SVR(C=c)
-                    svr_c_init_vc = SVR(gamma=g)
+                    #knn_init_vc = KNeighborsRegressor(weights='distance')
+                    #rr_init_vc = Ridge()
+                    #svr_g_init_vc = SVR(C=c)
+                    #svr_c_init_vc = SVR(gamma=g)
                     knn_init_lc = KNeighborsRegressor(n_neighbors=k, weights='distance')
                     rr_init_lc = Ridge(alpha=a)
                     svr_init_lc = SVR(gamma=g, C=c)
                 else:
                     scores = ['accuracy', ]
                     kfold = StratifiedKFold(n_splits=CV, shuffle=True)
-                    knn_init_vc = KNeighborsClassifier(weights='distance')
-                    rr_init_vc = RidgeClassifier(class_weight='balanced')
-                    svr_g_init_vc = SVC(C=c, class_weight='balanced')
-                    svr_c_init_vc = SVC(gamma=g, class_weight='balanced')
+                    #knn_init_vc = KNeighborsClassifier(weights='distance')
+                    #rr_init_vc = RidgeClassifier(class_weight='balanced')
+                    #svr_g_init_vc = SVC(C=c, class_weight='balanced')
+                    #svr_c_init_vc = SVC(gamma=g, class_weight='balanced')
                     knn_init_lc = KNeighborsClassifier(n_neighbors=k, weights='distance')
                     rr_init_lc = RidgeClassifier(alpha=a, class_weight='balanced')
                     svr_init_lc = SVC(gamma=g, C=c, class_weight='balanced')
                 
                 csv_name = 'trainset_' + trainset + '_' + subset + '_' + parameter + '_'
                 # Run one diagnostic curve at a time
-                validation_curves(trainX, trainY, knn_init_vc, rr_init_vc, 
-                                  svr_g_init_vc, svr_c_init_vc, scores, kfold, 
-                                  csv_name)
-                #learning_curves(trainX, trainY, knn_init_lc, rr_init_lc, 
-                #                svr_init_lc, scores, kfold, csv_name)
+                #validation_curves(trainX, trainY, knn_init_vc, rr_init_vc, 
+                #                  svr_g_init_vc, svr_c_init_vc, scores, kfold,
+                #                  csv_name)
+                learning_curves(trainX, trainY, knn_init_lc, rr_init_lc,
+                                svr_init_lc, scores, kfold, csv_name)
 
-            print("The {} predictions in trainset {} are complete\n".format(subset, trainset), flush=True)
+                print("The {} {} predictions in trainset {} are complete\n".format(subset, parameter, trainset), flush=True)
 
 if __name__ == "__main__":
     main()
