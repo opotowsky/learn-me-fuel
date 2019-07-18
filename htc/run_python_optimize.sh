dab4a3821667eab19c07bb382a16229acfa3e472
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2019-07-18 11:54:22 -0500

    files for optimization condor jobs

---
+++ htc/run_python_optimize.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# untar Python installation
+tar -xzf python.tar.gz
+
+# make sure the script will use Python installation
+# and the working directory as it's home location
+export PATH=$(pwd)/python/bin:$PATH
+mkdir home
+export HOME=$(pwd)/home
+# run your script
+python optimize_hyperparameters.py $1 $2
