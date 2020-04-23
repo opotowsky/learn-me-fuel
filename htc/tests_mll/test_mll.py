19c75897d83fd616aecbe8ff8cf15c6764f65c51
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2020-04-23 11:01:41 -0500

    restructuring and adding structure for testing mll code. it only exists in this htc directory so the tests dir is named to show it's not also testing the scikit dir

---
+++ htc/tests_mll/test_mll.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python3
+
+#from mll-calc.mll-calc import *
+
+#import pytest
+#import os
+#import pickle
+#import numpy as np
+#import pandas as pd
+
+
+def test_test():
+    exp = 1
+    obs = 1
+    assert obs == exp
+
