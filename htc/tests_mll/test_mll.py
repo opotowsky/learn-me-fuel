309283c112a83b01682bb188fdb261492289622c
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2020-05-01 14:36:26 -0500

    dummy test

--- htc/tests_mll/test_mll.py
+++ htc/tests_mll/test_mll.py
@@ -1,16 +1,20 @@
 #! /usr/bin/env python3
 
-#from mll-calc.mll-calc import *
+from mll_calc.mll_calc import *
 
+import math
 #import pytest
 #import os
 #import pickle
 #import numpy as np
-#import pandas as pd
+import pandas as pd
 
 
-def test_test():
-    exp = 1
-    obs = 1
+def test_like_calc():
+    y_sim = pd.Series([1, 1, 1, 1])
+    y_mes = pd.Series([1, 1, 1, 1])
+    std = pd.Series([1, 1, 1, 1])
+    exp = (1 / math.sqrt(2 * math.pi)) ** len(y_sim)
+    obs = like_calc(y_sim, y_mes, std)
     assert obs == exp
 
