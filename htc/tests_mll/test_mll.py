8b95e0bddcb1849d5146eda47eca79ed69d3cdce
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2020-05-12 12:35:46 -0500

    adds test for get_pred

--- htc/tests_mll/test_mll.py
+++ htc/tests_mll/test_mll.py
@@ -2,16 +2,19 @@
 
 from mll_calc.mll_calc import *
 
-import math
 import numpy as np
 import pandas as pd
+
+def ll_calc(x):
+    ll = -0.5 * (x**2 + np.log(2 * np.pi))
+    return ll
 
 # dummy test (don't need to test single line scipy func, I think)
 def test_like_calc():
     y_sim = pd.Series([1, 1, 1, 1])
     y_mes = pd.Series([1, 1, 1, 1])
     std = pd.Series([1, 1, 1, 1])
-    exp = (1 / math.sqrt(2 * math.pi)) ** len(y_sim)
+    exp = (1 / np.sqrt(2 * np.pi)) ** len(y_sim)
     obs = like_calc(y_sim, y_mes, std)
     assert obs == exp
 
@@ -27,21 +30,38 @@ def test_ratios():
     obs = ratios(XY, ratio_list, labels)
     assert obs.equals(exp)
 
-def test_testset_mll_XY():
-    XY = pd.DataFrame({'A' : [0, 0, 0, 0], 
-                       'B' : [0, 0, 0, 0],
-                       'C' : [0, 0, 0, 0],
-                       'label' : [1, 1, 1, 1]})
-    test = XY.copy()
+def test_get_pred():
+    XY = pd.DataFrame({'feature' : [1, 3], 
+                       'label' : ['Y', 'Z']},
+                       index = [1, 2])
+    test_sample = pd.Series([0], index=['feature'])
     unc = 1
     lbls = ['label']
-    exp_1 = pd.DataFrame({'A' : [1., 2., 3., np.nan], 
-                          'B' : [1., 1., 0., 0],
-                          'label' : [1, 1, 1, 1]})
-    exp_2 = ['Pred_label']
-    obs_1, obs_2 = testset_mll(XY, test, unc, lbls)
+    ll_name = 'LogLikelihood_' + str(unc)
+    exp_1 = pd.DataFrame({'pred_label' : ['Y'],
+                          ll_name : [ll_calc(1)]},
+                          index = [1])
+    exp_2 = ['pred_label', ll_name]
+    obs_1, obs_2 = get_pred(XY, test_sample, unc, lbls)
     assert obs_1.equals(exp_1)
     assert obs_2 == exp_2
+
+#def test_testset_mll_XY():
+#    XY = pd.DataFrame({'feature' : [0, 1, 3], 
+#                       'label' : ['X', 'Y', 'Z']})
+#    test = XY.copy()
+#    unc = 1
+#    lbls = ['label']
+#    ll_name = 'LogLikelihood_' + str(unc)
+#    exp_1 = pd.DataFrame({'sim_idx' : [0, 1, 2],
+#                          'label' : [0, 1, 2],
+#                          'pred_idx' : [1, 0, 1],
+#                          'pred_label' : ['Y', 'X', 'Y'],
+#                          ll_name : [ll_calc(1), ll_calc(1), ll_calc(2)]})
+#    exp_2 = ['pred_label', ll_name]
+#    obs_1, obs_2 = testset_mll(XY, test, unc, lbls)
+#    assert obs_1.equals(exp_1)
+#    assert obs_2 == exp_2
 
 #def test_testset_mll_ext():
 #
