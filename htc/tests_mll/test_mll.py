0cacc66c0b2e5beee5b0043ccf4db6c2c9d972c2
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2020-06-30 15:31:36 -0500

    updating tests due to recent changes, in progress

--- htc/tests_mll/test_mll.py
+++ htc/tests_mll/test_mll.py
@@ -1,6 +1,6 @@
 #! /usr/bin/env python3
 
-from mll_calc.mll_calc import format_XY, ratios, get_pred, mll_testset, parse_args
+from mll_calc.mll_calc import unc_calc, format_XY, ratios, format_pred, ll_cdf, get_pred, mll_testset, parse_args
 import pickle
 import pytest
 import numpy as np
@@ -22,14 +22,22 @@ def calc_ll_exp(x, std):
     ll = -0.5 * ((x / std)**2 + np.log(2 * np.pi) + 2 * np.log(std))
     return ll
 
+def test_unc_calc():
+    unc = 1
+    y1 = pd.Series([1, 2, np.nan, 0])
+    y2 = pd.Series([0, 1, np.nan, 10])
+    exp = np.sqrt(1 + 5/16)
+    obs = unc_calc(y1, y2, (unc * y1)**2, (unc * y2)**2)
+    assert obs == exp
+    
 def test_format_XY(tmpdir, dfXY):
     XY = pd.DataFrame({'feature' : [1, 2, 3],
                        'total' : [1, 2, 3],
-                       'Burnup' : [1, 1, 0]},
+                       'Burnup' : [1, 0, 1]},
                        index = [0, 1, 2])
     pkl_db = tmpdir.join('db.pkl')
     pickle.dump(XY, open(pkl_db, 'wb'))
-    exp = pd.DataFrame({'feature' : [1, 2], 
+    exp = pd.DataFrame({'feature' : [1, 3], 
                         'Burnup' : [1, 1]},
                         index = [0, 1])
     obs = format_XY(pkl_db)
@@ -47,61 +55,76 @@ def test_ratios():
     obs = ratios(XY, ratio_list, labels)
     assert obs.equals(exp)
 
-@pytest.mark.parametrize('sim_idx, exp',
-                         [(0, pd.DataFrame({'pred_label' : ['Y'], 'LL' : [calc_ll_exp(1, 2)]}, index = [1])),
-                          (1, pd.DataFrame({'pred_label' : ['X'], 'LL' : [calc_ll_exp(1, 1)]}, index = [0])),
-                          (2, pd.DataFrame({'pred_label' : ['Y'], 'LL' : [calc_ll_exp(1, 2)]}, index = [1]))
-                          ]
-                         )
-def test_get_pred(dfXY, sim_idx, exp):
-    XY, unc, lbls, ll_name = dfXY
-    test_sample = XY.loc[sim_idx].drop(lbls)
-    XY.drop(sim_idx, inplace=True)
-    #renaming LL col for now, until I understand parametrization with fixures
-    exp.rename(columns={'LL': ll_name}, inplace=True)
-    obs = get_pred(XY, test_sample, unc, lbls)
+def test_format_pred():
+    pred = pd.DataFrame({'nuc' : [1.], 'lbl1' : 'A', 'lbl2' : 1., 'll' : [1.]})
+    lbls = ['lbl1', 'lbl2']
+    ll_cols = ['ll']
+    exp = pd.DataFrame({'pred_lbl1' : 'A', 'pred_lbl2' : 1., 'll' : [1.]})
+    obs = format_pred(pred, lbls, ll_cols)
     assert obs.equals(exp)
+    
+#def test_ll_cdf():
+    #pred_ll = pd.DataFrame('nuc' : [1.], 'lbl1' : 'A', 'lbl2' : 1., 'll' : [1.])
+    #ll_df = 
+    #obs1, obs2 = ll_cdf(pred_ll, ll_df)
+    #assert obs1.equals(exp1)
+    #assert obs2 == exp2
 
-def test_mll_testset_XY(dfXY):
-    XY, unc, lbls, ll_name = dfXY
-    ext_test = False
-    test = XY.copy()
-    ll_exp = [calc_ll_exp(1, 2), calc_ll_exp(1, 1), calc_ll_exp(1, 2)]
-    exp = pd.DataFrame({'sim_idx' : [0, 1, 2],
-                          'label' : ['X', 'Y', 'Z'],
-                          'pred_idx' : [1, 0, 1],
-                          'pred_label' : ['Y', 'X', 'Y'],
-                          ll_name : ll_exp}, 
-                          index = [0, 1, 2])
-    obs = mll_testset(XY, test, ext_test, unc, lbls)
-    assert obs.equals(exp)
-
-def test_mll_testset_ext(dfXY):
-    XY, unc, lbls, ll_name = dfXY
-    ext_test = True
-    test = pd.DataFrame({'feature' : [4], 
-                       'label' : ['W']},
-                       index = ['A'])
-    ll_exp = [calc_ll_exp(1, 3)]
-    exp = pd.DataFrame({'sim_idx' : ['A'],
-                          'label' : ['W'],
-                          'pred_idx' : [2],
-                          'pred_label' : ['Z'],
-                          ll_name : ll_exp}, 
-                          index = [0])
-    obs = mll_testset(XY, test, ext_test, unc, lbls)
-    assert obs.equals(exp)
+#@pytest.mark.parametrize('sim_idx, exp',
+#                         [(0, pd.DataFrame({'pred_label' : ['Y'], 'LL' : [calc_ll_exp(1, 2)]}, index = [1])),
+#                          (1, pd.DataFrame({'pred_label' : ['X'], 'LL' : [calc_ll_exp(1, 1)]}, index = [0])),
+#                          (2, pd.DataFrame({'pred_label' : ['Y'], 'LL' : [calc_ll_exp(1, 2)]}, index = [1]))
+#                          ]
+#                         )
+#def test_get_pred(dfXY, sim_idx, exp):
+#    XY, unc, lbls, ll_name = dfXY
+#    test_sample = XY.loc[sim_idx].drop(lbls)
+#    XY.drop(sim_idx, inplace=True)
+#    #renaming LL col for now, until I understand parametrization with fixures
+#    exp.rename(columns={'LL': ll_name}, inplace=True)
+#    obs = get_pred(XY, test_sample, unc, lbls)
+#    assert obs.equals(exp)
+#
+#def test_mll_testset_XY(dfXY):
+#    XY, unc, lbls, ll_name = dfXY
+#    ext_test = False
+#    test = XY.copy()
+#    ll_exp = [calc_ll_exp(1, 2), calc_ll_exp(1, 1), calc_ll_exp(1, 2)]
+#    exp = pd.DataFrame({'sim_idx' : [0, 1, 2],
+#                          'label' : ['X', 'Y', 'Z'],
+#                          'pred_idx' : [1, 0, 1],
+#                          'pred_label' : ['Y', 'X', 'Y'],
+#                          ll_name : ll_exp}, 
+#                          index = [0, 1, 2])
+#    obs = mll_testset(XY, test, ext_test, unc, lbls)
+#    assert obs.equals(exp)
+#
+#def test_mll_testset_ext(dfXY):
+#    XY, unc, lbls, ll_name = dfXY
+#    ext_test = True
+#    test = pd.DataFrame({'feature' : [4], 
+#                       'label' : ['W']},
+#                       index = ['A'])
+#    ll_exp = [calc_ll_exp(1, 3)]
+#    exp = pd.DataFrame({'sim_idx' : ['A'],
+#                          'label' : ['W'],
+#                          'pred_idx' : [2],
+#                          'pred_label' : ['Z'],
+#                          ll_name : ll_exp}, 
+#                          index = [0])
+#    obs = mll_testset(XY, test, ext_test, unc, lbls)
+#    assert obs.equals(exp)
 
 @pytest.mark.parametrize('argv, exp',
-                         [(['0.05', 'xx', 'yy', 'zz', 'dir', '0', '1', '--ext-test', '--ratios'], 
-                           [0.05, 'xx', 'yy', 'zz', 'dir', [0, 1], True, True]
+                         [(['dir', '0.05', 'xx', 'yy', 'zz', '0', '--ext-test', '--ratios'], 
+                           ['dir', 0.05, 'xx', 'yy', 'zz', 0, True, True]
                            ),
-                          (['0.05', 'xx', 'yy', 'zz', 'dir', '0', '1', '--no-ext-test', '--no-ratios'], 
-                           [0.05, 'xx', 'yy', 'zz', 'dir', [0, 1], False, False]
+                          (['dir', '0.05', 'xx', 'yy', 'zz', '0', '--no-ext-test', '--no-ratios'], 
+                           ['dir', 0.05, 'xx', 'yy', 'zz', 0, False, False]
                            )
                           ]
                          )
 def test_parse_args(argv, exp):
     args = parse_args(argv)
-    obs = [args.sim_unc, args.train_db, args.test_db, args.outfile, args.outdir, args.db_rows, args.ext_test, args.ratios]
+    obs = [args.outdir, args.sim_unc, args.train_db, args.test_db, args.outfile, args.db_row, args.ext_test, args.ratios]
     assert obs == exp
--- htc/tests_mll/test_mll_integration.py
+++ htc/tests_mll/test_mll_integration.py
@@ -5,23 +5,21 @@ import pytest
 import subprocess
 import numpy as np
 import pandas as pd
+from pathlib import Path
 
 @pytest.mark.parametrize('exp, bool_argv',
-                         [(11, ['--no-ext-test', '--no-ratios']),
-                          (3, ['--ext-test', '--no-ratios']),
-                          (11, ['--no-ext-test', '--ratios']),
-                          (3, ['--ext-test', '--ratios'])
+                         [(10, ['--no-ext-test', '--no-ratios']),
+                          (2, ['--ext-test', '--no-ratios']),
+                          (10, ['--no-ext-test', '--ratios']),
+                          (2, ['--ext-test', '--ratios'])
                           ]
                          )
 def test_integration(tmpdir, exp, bool_argv):
-    const_argv = ['0.05', './tests_mll/sample10sim.pkl', './tests_mll/sfcompo2.pkl'] 
-    htc_argv = ['JobDir', '0', '10']
-    outfile = tmpdir.join('output')
-    cmd_list = ['./mll_calc/mll_calc.py'] + const_argv + [outfile] + htc_argv + bool_argv
-    subprocess.run(cmd_list)
-    # Just testing # of lines in final output for now
-    with open(outfile + '.csv', 'r') as f: 
-        reader = csv.reader(f)
-        obs_lines = len(list(reader))
-    assert obs_lines == exp
+    const_argv = ['JobDir', '0.05', './tests_mll/sample10sim.pkl', './tests_mll/sfcompo2.pkl']
+    for row in range(0, exp):
+        outfile = tmpdir.join('output' + str(row))
+        cmd_list = ['./mll_calc/mll_calc.py'] + const_argv + [row] + bool_argv
+        subprocess.run(cmd_list)
+    # Just testing # of csv files created for now
+    assert len(list(Path(tmpdir).glob('*.csv'))) == exp
 
