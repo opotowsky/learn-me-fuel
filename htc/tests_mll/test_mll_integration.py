478947d71cf0dfd36df5c5dbbcf6d9429fac7e75
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2020-07-01 11:17:46 -0500

    fixed integration tests for new argparse scheme

--- htc/tests_mll/test_mll_integration.py
+++ htc/tests_mll/test_mll_integration.py
@@ -18,7 +18,7 @@ def test_integration(tmpdir, exp, bool_argv):
     const_argv = ['JobDir', '0.05', './tests_mll/sample10sim.pkl', './tests_mll/sfcompo2.pkl']
     for row in range(0, exp):
         outfile = tmpdir.join('output' + str(row))
-        cmd_list = ['./mll_calc/mll_calc.py'] + const_argv + [row] + bool_argv
+        cmd_list = ['./mll_calc/mll_calc.py'] + const_argv + [outfile, str(row)] + bool_argv
         subprocess.run(cmd_list)
     # Just testing # of csv files created for now
     assert len(list(Path(tmpdir).glob('*.csv'))) == exp
