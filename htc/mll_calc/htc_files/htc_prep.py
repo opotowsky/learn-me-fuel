ca961b638c18fe0bfcf8978e9c0fdf01f32bb799
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2020-07-10 15:54:01 -0500

    finished htc_prep.py, and params.txt files successfully created

--- htc/mll_calc/htc_files/htc_prep.py
+++ htc/mll_calc/htc_files/htc_prep.py
@@ -1,8 +1,9 @@
 #! /usr/bin/env python3
 
-import os
+import sys
 import csv
-import subprocess
+import numpy as np
+import pandas as pd
 from all_jobs import parent_jobs, kid_jobs
 
 def format_db(db_path):
@@ -14,33 +15,41 @@ def format_db(db_path):
     XY.reset_index(inplace=True, drop=True)
     return XY
 
-def row_calcs(ext_test, pkls, n_rows):
+def row_calcs(ext_test, n_rows):
+    ################################################
+    # local filepaths corresponding to DBs in HTC: #
+    tamu_db = '~/sims_n_results/nucmoles_opusupdate_aug2019/not-scaled_15nuc.pkl'
+    sfco_db = '~/sfcompo/format_clean/sfcompo_formatted.pkl'
+    ################################################
+    
     if 'no' in ext_test:
-        test_set = format_db(kid_jobs['pkls'][0])
+        test_set = format_db(tamu_db)
     else:
-        test_set = format_db(kid_jobs['pkls'][1])
-    db_rows = len(XY.index)
+        test_set = format_db(sfco_db)
+    db_rows = len(test_set.index)
+    init_rows = np.arange(0, db_rows, n_rows).tolist()
+    end_rows = init_rows[1:]
+    end_rows.append(db_rows+1)
     
     ########################
     #### In-script test ####
     ########################
-    
-    total_jobs = db_rows // 10 + 1
-
-    
+    total_jobs = db_rows // n_rows + 1
+    if len(init_rows) != total_jobs or len(end_rows) != total_jobs:
+        sys.exit('total expected jobs does not equal one of db_row lists')
     ########################
 
     return init_rows, end_rows
 
 def make_paramstxt(parent_job, kid_jobs):
-    init_rows, end_rows = row_calcs(parent_job['ext_test'], kid_jobs['pkls'], kid_jobs['rows_per_job'])
+    init_rows, end_rows = row_calcs(parent_job['ext_test'], kid_jobs['rows_per_job'])
     parent_dir = parent_job['parent_dir']
     fname = parent_dir + '_params.txt'
     with open(fname, 'w') as f:
         w = csv.writer(f) 
         for kid_dir, unc in zip(kid_jobs['job_dirs'], kid_jobs['uncs']):
             job_dir = parent_dir + '/' + kid_dir
-            for i in range(0, len(init_row)):
+            for i in range(0, len(init_rows)):
                 init_row = init_rows[i]
                 end_row = end_rows[i]
                 job = [job_dir, unc, 
@@ -51,26 +60,13 @@ def make_paramstxt(parent_job, kid_jobs):
                 w.writerow(job)    
     return
 
-def make_dirs(parent_dir, kid_dirs):
-    if not os.path.isdir(parent_dir):
-        subprocess.run(['mkdir', parent_dir])
-    for kid_dir in kid_dirs:
-        job_dir = parent_dir + '/' + kid_dir
-        if not os.path.isdir(job_dir):
-            subprocess.run(['mkdir', job_dir])
-    return
-
 def main():
     """
-    Reads all the job descriptions from all_jobs.py and completes two 
-    prep tasks:
-
-    1. Makes directories for each job for HTC output
-    2. Populates the necessary params_mll_calc.txt files
+    Reads all the job descriptions from all_jobs.py and populates the necessary
+    params_mll_calc.txt files
     
     """
     for parent_job in parent_jobs:
-        make_dirs(parent_job['parent_dir'], kid_jobs['job_dirs'])
         make_paramstxt(parent_job, kid_jobs)
     return
     
