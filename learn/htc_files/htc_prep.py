0b7d2c560eb3d94236d6c3e8336b8b3aeddb31f9
Arrielle Opotowsky <arrielle.opotowsky@gmail.com>
2021-04-21 18:51:12 -0400

    some changes to htc_prep for new processed spectra trainset, still a bit of a messy file!

--- learn/htc_files/htc_prep.py
+++ learn/htc_files/htc_prep.py
@@ -2,19 +2,24 @@
 
 import csv
 
-def make_paramstxt(train, txtfile):
+def make_paramstxt(train, txtfile, file_descrip):
     
     rxtr_param = ['reactor', 'cooling', 'enrichment', 'burnup']
     algs = ['knn', 'dtree']
+    # all funcs:
     #func_type = ['--track_preds', '--err_n_scores', '--learn_curves', 
     #             '--valid_curves', '--test_compare', '--random_error']
-    func_type = ['--test_compare',]# '--random_error']
+    # funcs ran for nuc conc scenario:
+    #func_type = ['--test_compare', '--random_error']
+    # funcs ran for gamma spec scenario:
+    func_type = ['--random_error',]
     tset_frac = [0.1, 0.3, 1.0]
     cv = 5
 
     for param in rxtr_param:
         for func in func_type:
             # test_compare or blank args
+            # only applicable in nuc conc scenario so leaving as-is
             if 'test_compare' in func:
                 filename = 'sfco_compare' + txtfile
                 ts_flag = '--testing_set'
@@ -29,10 +34,7 @@ def make_paramstxt(train, txtfile):
             for alg in algs:
                 for frac in tset_frac:
                     # outfile naming
-                    if '15' in train:
-                        outfile = param + '_' + alg + '_tset' + str(frac) + '_nuc15'
-                    else:
-                        outfile = param + '_' + alg + '_tset' + str(frac) + '_nuc29'
+                    outfile = param + '_' + alg + '_tset' + str(frac) + file_descrip
                     with open(filename, 'a') as f:
                         w = csv.writer(f)
                         job = [outfile, param, alg, str(frac), str(cv),
@@ -42,14 +44,21 @@ def make_paramstxt(train, txtfile):
 
 def main():
     """
-    Populates the necessary param_nucXX.txt files
+    Populates the necessary htc param.txt files
     
     """
-    train_db = ['sim_grams_nuc15.pkl', 'sim_grams_nuc29.pkl']
-    txtfile = ['_nuc15_param.txt', '_nuc29_param.txt']
+    # nuclide concentration lists
+    #dblist = ['sim_grams_nuc15.pkl', 'sim_grams_nuc29.pkl']
+    #txtfile = ['_nuc15_param.txt', '_nuc29_param.txt']
+    #outfile = ['_nuc15', '_nuc29']
     
-    for train, tfile in zip(train_db, txtfile):
-        make_paramstxt(train, tfile)
+    # activity lists
+    dblist = ['test.pkl',]
+    txtfile = ['_d1_param.txt',]
+    outfile = ['_d1_hpge',]
+
+    for i, traindb in enumerate(dblist):
+        make_paramstxt(traindb, txtfile[i], outfile[i])
     return
     
 if __name__ == "__main__":
